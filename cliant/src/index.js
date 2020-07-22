da3b14a783c4e6b5d7ea1a601b84a27652473afc
fukunagaa <k.k.0627ku@gmail.com>
2020-07-22 18:29:53 +0900

    react-redux(Provider)の使用

--- cliant/src/index.js
+++ cliant/src/index.js
@@ -1,23 +1,27 @@
-  import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import Layout from "./components/layout/Layout";
 import Home from "./components/home/Home";
 import Favorites from "./components/favorites/Favorites";
 import TodoApp from "./components/todo/TodoApp";
-import Settings from "./components/settings/Settings"
+import Settings from "./components/settings/Settings";
 
 const app = document.getElementById("app");
 
 ReactDOM.render(
-  <Router>
-    <Layout>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/todos/:article" component={TodoApp}></Route>
-      <Route path="/favorites/:article" component={Favorites}></Route>
-      <Route path="/settings/:mode(main|extra)" component={Settings}></Route>
-    </Layout>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/todos/:article" component={TodoApp}></Route>
+        <Route path="/favorites/:article" component={Favorites}></Route>
+        <Route path="/settings/:mode(main|extra)" component={Settings}></Route>
+      </Layout>
+    </Router>
+  </Provider>,
   app
 );
