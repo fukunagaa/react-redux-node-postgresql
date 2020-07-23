b6b6245b0c0fde17bdea43ff08a793a8edc46b36
fukunagaa <k.k.0627ku@gmail.com>
2020-07-23 15:58:23 +0900

    toggle関数の残りを削除

--- cliant/src/components/todo/Todo.js
+++ cliant/src/components/todo/Todo.js
@@ -3,11 +3,10 @@ import className from "classnames";
 
 import { STATUS_DONE } from "../../utils/constants";
 
-const Todo = ({ todo, index, toggleTodo, dragStart }) => (
+const Todo = ({ todo, index }) => (
   <li
     className={"list-style-none"}
     id={index}
-    onClick={() => toggleTodo(index)}
     draggable="true"
     onDragStart={() => event.dataTransfer.setData("id", event.target.id)}
   >
