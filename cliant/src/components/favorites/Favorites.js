2e40acea37da17ef9f3fa6cd21dfffaba6602c4d
fukunagaa <k.k.0627ku@gmail.com>
2020-07-23 16:55:07 +0900

    Favoriteのみの表示

--- cliant/src/components/favorites/Favorites.js
+++ cliant/src/components/favorites/Favorites.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import Todo from "../todo/Todo";
+import { FAVORITE_YES } from "../../utils/constants";
 
-class Favorites extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Favorites</h1>
-      </div>
-    );
-  }
-}
+const Favorites = ({ doingsIds, byIds }) => (
+  <div className={"todo-list-all-container"}>
+    <h1>Favorites</h1>
+    <ul>
+      {doingsIds && doingsIds
+        ? doingsIds.map((index) => {
+            return (
+              <Todo key={`todo-${index}`} index={index} todo={byIds[index]} />
+            );
+          })
+        : "NO todos, yay!"}
+    </ul>
+  </div>
+);
 
-export default Favorites;
+const mapStateToProps = (state) => {
+  const todos = state.todos;
+  const byIds = state.todos.byIds;
+  let doingsIds = [];
+  todos.allIds.map((index) => {
+    console.log(todos.byIds[index].status);
+    if (todos.byIds[index].favorite == FAVORITE_YES) {
+      doingsIds.push(index);
+    }
+  });
+  return { doingsIds, byIds };
+};
+
+export default connect(mapStateToProps)(Favorites);
