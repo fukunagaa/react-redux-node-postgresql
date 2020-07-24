038a88d7421d037bbf432279818ce0a36e29f66b
fukunagaa <k.k.0627ku@gmail.com>
2020-07-24 21:37:59 +0900

    Todoの定数 promise化に伴う修正

--- cliant/src/redux/actionTypes.js
+++ cliant/src/redux/actionTypes.js
@@ -1,4 +1,7 @@
 //==================todo==================
 export const ADD_TODO = "ADD_TODO";
+export const ADD_TODO_PENDING = "ADD_TODO_PENDING";
+export const ADD_TODO_FULFILLED = "ADD_TODO_FULFILLED";
+export const ADD_TODO_REJECTED = "ADD_TODO_REJECTED";
 export const DROP_TODO = "DROP_TODO";
 export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
